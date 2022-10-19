c8efdf9c2776a7020df3ab08c5db1a39bfab94b8
Jorge-Marquez-16 <marquezrubioja@gmail.com>
2022-10-19 11:54:45 -0500

    Test

---
+++ opencv/vision_op1/build/CMakeFiles/vision_op1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vision_op1.dir/src/main.cpp.o"
+  "vision_op1"
+  "vision_op1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vision_op1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
